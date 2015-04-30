9af67035ac12fd65a070e3ad153753ce9ee64b8b
Iris <irismeerman@gmail.com>
2015-04-30 17:55:12 +0200

    met comments

--- time.pl
+++ time.pl
@@ -1,6 +1,14 @@
 % Kennissystemen opdracht 4
 % Common Sense Reasoning
 
+% Idee: 
+% 1. Verzamel alle 'before' relaties (findall)
+% 2. Zet alle gevonden events in een lijst (a before b. b before c wordt([a,b,c]))
+% 3. Remove dubbele events in de lijst somehow
+% 4. Verzamel alle 'concurrent' relaties (findall)
+% 5. Op iedere plek waar een element staat dat voorkomt in een 'concurrent' predicaat,
+% 	 plaats daar een lijst met de concurrent events.
+
 :- op(700, xfx, before).
 :- op(700, xfx, after).
 :- op(700, xfx, concurrent).
@@ -10,6 +18,7 @@
 :- dynamic(event/1).
 :- dynamic(relation/1).
 
+% start predicaat voor het invoegen van kennis aan de knowledgebase.
 ask:-
     read(UserInput),
     parse(UserInput), !,
@@ -19,14 +28,17 @@ ask:-
 ask:-
 	write('Dit staat al in de knowledgebase. Probeer opnieuw.\n').
 
+% zet 'after'-input om naar 'before' input en voeg toe aan knowledgebase.
 parse(relation(X after Y)):-
 	checkDuplicates(relation(Y before X)), !, 
   	assert(relation(Y before X)).	
 
+% check of de input al in de knowledgebase staat.
 parse(UserInput):-
   	checkDuplicates(UserInput), !, 
   	assert(UserInput).
 
+% pas transiviteitsregel toe op before events
 trans:-
 	event(X), event(Y), event(Z),
 	X \= Y, X \= Z, Y \= Z,
@@ -37,6 +49,7 @@ trans:-
 	write(relation(X before Z)),
 	write(' is toegevoegd aan de database.\n') ,!, trans. 
 
+% pas transiviteitsregel toe op concurrent events
 trans:-
 	event(X), event(Y), event(Z),
 	X \= Y, X \= Z, Y \= Z,
@@ -50,6 +63,8 @@ trans:-
 trans:-
 	write('Geen transitieve relaties meer om toe te voegen.\n'), !.
 
+% kijk of de input al in de knowledgebase staat en of het een geldige
+% input regel is.
 checkDuplicates(Input):-
 	\+ Input,
 	((Input = relation(X concurrent Y),
@@ -95,6 +110,9 @@ rewrite(X before_or_concurrent Y):-
 rewrite(X concurrent_or_after Y):-
 	rewrite(X concurrent Y), 
 	rewrite(X after Y).
+
+%%%%%%%%%%%%%%%%%%%%% BUILD IN INFO %%%%%%%%%%%%%%%%%%%
+
 
 go1:-
 	assert(event('a')),
